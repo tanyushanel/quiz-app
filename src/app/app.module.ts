c4e5ab6350dcc04b7d6d0ad2e625993db06f0d53
tanyushanel <tanyushanel@users.noreply.github.com>
2020-09-29 15:41:26 +0300

    routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PreviewPageComponent } from './preview-page/preview-page.component';
 import { ScorePageComponent } from './score-page/score-page.component';
 import { StartPageComponent } from './start-page/start-page.component';
 import { QuestionPageComponent } from './question-page/question-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: StartPageComponent },
+  { path: 'question', component: QuestionPageComponent },
+  { path: 'preview', component: PreviewPageComponent },
+  { path: 'score', component: ScorePageComponent },
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +23,7 @@ import { QuestionPageComponent } from './question-page/question-page.component';
     StartPageComponent,
     QuestionPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
