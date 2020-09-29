4ce1b2f38021a2efdf8d4df2171c65d15aaeadd3
tanyushanel <tanyushanel@users.noreply.github.com>
2020-09-29 15:24:22 +0300

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreviewPageComponent } from './preview-page/preview-page.component';
+import { ScorePageComponent } from './score-page/score-page.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { QuestionPageComponent } from './question-page/question-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviewPageComponent,
+    ScorePageComponent,
+    StartPageComponent,
+    QuestionPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
